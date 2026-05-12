4b9dc8a5990d2abfc8840d0aa38231fd79977fad
Radu-A <elcastravete@gmail.com>
2026-05-12 09:19:52 +0200

    feat(libros): create entity and import.sql

---
+++ src/main/java/com/github/Radu_A/proyecto_07/model/Libro.java
@@ -0,0 +1,58 @@
+package com.github.Radu_A.proyecto_07.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String autor;
+	@Column(name = "numero_paginas")
+	private Integer numeroPaginas;
+	
+	// constructor
+	public Libro() {}
+
+	// getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public void setNumeroPaginas(Integer numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+	
+}
