4e4a36eafa7909087e4f436ad22310d25a1a9627
Radu-A <elcastravete@gmail.com>
2026-05-12 08:54:43 +0200

    first commit

---
+++ src/test/java/com/github/Radu_A/proyecto_07/Proyecto07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.proyecto_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
